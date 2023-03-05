4d94fd46f1457cec7e9eb6526e5ee03b0ea8dc10
Gary Leong <gary@thyruth.com>
2023-03-05 22:18:11 +0800

    'update terraform version'

--- execgroups/_ed_configs/rds/_chrootfiles/var/tmp/terraform/provider.tf
+++ execgroups/_ed_configs/rds/_chrootfiles/var/tmp/terraform/provider.tf
@@ -1,12 +1,22 @@
 provider "aws" {
   region  = var.aws_default_region
+  default_tags {
+    tags = merge(
+      var.cloud_tags,
+      {
+        orchestrated_by = "elasticdev"
+      },
+    )
+  }
 }
 
 terraform {
+  required_version = ">= 1.1.0"
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "~> 2.57.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
 }
